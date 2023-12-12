3ad8535eade9947c68212e20ab0525cb7460f483
oneminute3b <oneminute3b@gmail.com>
2023-12-12 08:37:12 -0600

    adds ListTesting.java

---
+++ gramma/Pre/ListTesting.java
@@ -0,0 +1,11 @@
+package gramma.Pre;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListTesting {
+    
+    public static void main(String[] args) {
+        List<int> list1 = new ArrayList<int>();
+    }
+}
